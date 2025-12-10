f76347e1a01070454c64a73294c67e6e9052dd96
Basma Elmahany <basmakhalaf974@gmail.com>
2025-12-10 11:09:15 +0200

    update

---
+++ Models/UpdateJournalDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+    public class UpdateJournalDto
+    {
+        public string EntryNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public IFormFile? Photo { get; set; }
+        public string LinesJson { get; set; }
+    }
+}
